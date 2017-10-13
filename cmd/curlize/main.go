69c863661c177fdedd9077ccbd6cf6572a2c524d
agatan <agatan039@gmail.com>
2017-10-13 17:10:08 +0900

    Reverse Proxy mode

--- cmd/curlize/main.go
+++ cmd/curlize/main.go
@@ -4,6 +4,8 @@ import (
 	"flag"
 	"log"
 	"net/http"
+	"net/http/httputil"
+	"net/url"
 
 	"github.com/agatan/curlize"
 	"github.com/agatan/groxy"
@@ -11,12 +13,14 @@ import (
 
 type options struct {
 	address string
+	reverse string
 }
 
 func main() {
 	var op options
 
 	flag.StringVar(&op.address, "addr", ":8080", "listening address")
+	flag.StringVar(&op.reverse, "reverse", "", "upstream address for reverse proxy mode")
 
 	flag.Parse()
 
@@ -35,7 +39,32 @@ func main() {
 		}
 	})
 
+	if op.reverse != "" {
+		upstream, err := url.Parse(op.reverse)
+		if err != nil {
+			panic(err)
+		}
+		proxy.NonProxyRequestHandler = reverseProxyHandler(upstream)
+	}
+
 	if err := http.ListenAndServe(op.address, proxy); err != nil {
 		panic(err)
 	}
+}
+
+func reverseProxyHandler(upstream *url.URL) http.Handler {
+	reverseProxy := httputil.NewSingleHostReverseProxy(upstream)
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		u := *upstream
+		u.Path = r.URL.Path
+		r.URL = &u
+		r.Host = u.Host
+		curl, err := curlize.Curlize(r)
+		if err != nil {
+			log.Println(err)
+		} else {
+			log.Println(curl.String())
+		}
+		reverseProxy.ServeHTTP(w, r)
+	})
 }
